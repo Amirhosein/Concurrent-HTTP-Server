06803ead08ec325d7a7283b30b6e4d1b4e6723a7
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2022-07-22 23:14:02 +0430

    feat: Implement Error and Successful upload response

--- internal/response/response.go
+++ internal/response/response.go
@@ -3,3 +3,11 @@ package response
 type Message struct {
 	Message string `json:"message" binding:"required"`
 }
+
+type Error struct {
+	Error string `json:"error" binding:"required"`
+}
+
+type SuccessfulUpload struct {
+	FileId string `json:"file_id"`
+}
