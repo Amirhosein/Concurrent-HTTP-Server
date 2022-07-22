99b7c7dd9e4ec6cedd331d5fbceea052aac3e8a5
Amirhossein Iravanimanesh <airavanimanesh@gmail.com>
2022-07-22 23:14:50 +0430

    feat: Implement hash and encoding functions

---
+++ internal/pkg/hash.go
@@ -0,0 +1,29 @@
+package pkg
+
+import (
+	"crypto/sha256"
+	"log"
+	"math/big"
+
+	uuid "github.com/nu7hatch/gouuid"
+)
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+
+	return algorithm.Sum(nil)
+}
+
+func GenerateFileId(filename string) uint64 {
+	u, err := uuid.NewV4()
+	if err != nil {
+		panic(err)
+	}
+
+	urlHashBytes := sha256Of(filename + u.String())
+	id := new(big.Int).SetBytes(urlHashBytes).Uint64()
+
+	log.Println(id)
+	return id
+}
